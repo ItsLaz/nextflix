509367febb43fefc2c6bca2d0333664691320c47
ItsLaz <yesimandriy@gmail.com>
2022-04-08 20:49:28 -0500

    useRouter, dropdown functionality on username, some style fixes

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,28 +1,63 @@
 import styles from './Navbar.module.css';
+
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { useState } from 'react';
+
 const Navbar = (props) => {
     const { username } = props;
+    const router = useRouter();
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleOnClickHome = (e) => {
+        e.preventDefault();
+        router.push('/');
+    };
+    const handleOnClickList = (e) => {
+        e.preventDefault();
+        router.push('/browse/my-list');
+    };
+    const handleShowDropdown = (e) => {
+        e.preventDefault();
+        setShowDropdown(!showDropdown);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <a className={styles.logoLink} href="/">
+                <Link className={styles.logoLink} href="/" passHref>
                     <div className={styles.logoWrapper}>Netflix</div>
-                </a>
+                </Link>
 
                 <ul className={styles.navItems}>
-                    <li className={styles.navItem1}>Home</li>
-                    <li className={styles.navItem2}>My List</li>
+                    <li className={styles.navItem1} onClick={handleOnClickHome}>
+                        Home
+                    </li>
+                    <li className={styles.navItem2} onClick={handleOnClickList}>
+                        My List
+                    </li>
                 </ul>
                 <nav className={styles.navContainer}>
                     <div>
-                        <button className={styles.usernameBtn}>
+                        <button
+                            className={styles.usernameBtn}
+                            onClick={handleShowDropdown}
+                        >
                             <p className={styles.username}>{username}</p>
                         </button>
-                        <div className={styles.navDropdown}>
-                            <div>
-                                <a className={styles.linkName}>Sign out</a>
-                                <div className={styles.lineWrapper}></div>
+                        {showDropdown && (
+                            <div className={styles.navDropdown}>
+                                <div>
+                                    <Link href="/login">
+                                        <a className={styles.linkName}>
+                                            Sign out
+                                        </a>
+                                    </Link>
+                                    <div className={styles.lineWrapper}></div>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </nav>
             </div>
